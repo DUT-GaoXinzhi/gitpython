15917c1614819422ab57e5a1f7f739906c38ec37
DUT_gaoxinzhi <651599228@qq.com>
2019-12-21 23:53:10 +0800

    update

---
+++ pytorch/surgical_dataset_4classes/newdata.py
@@ -0,0 +1,53 @@
+import cv2
+import torch
+import os
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+
+class MyDataset(torch.utils.data.Dataset):  # 创建自己的类：MyDataset,这个类是继承的torch.utils.data.Dataset
+    def __init__(self, train_root, transform=None, target_transform=None):  # 初始化一些需要传入的参数
+        super(MyDataset, self).__init__()
+        imgs = []  # 创建一个名为img的空列表，一会儿用来装东西
+        self.train_root =train_root
+        class_list = ["forceps1/", "scissors1/", "scissors2/", "tweezers/"]
+        label = -1
+        for name in class_list:
+            label += 1
+            data_path = self.train_root + name
+            for img in os.listdir(data_path):
+                img_path = data_path + img
+                imgs.append((img_path, label))
+        self.imgs = imgs
+        self.transform = transform
+        self.target_transform = target_transform
+    def __getitem__(self, index):
+        #这个方法是必须要有的，用于按照索引读取每个元素的具体内容
+        fn, label = self.imgs[index]  # fn是图片path #fn和label分别获得imgs[index]也即是刚才每行中word[0]和word[1]的信息
+        img = cv2.imread(fn)
+        if self.transform is not None:
+            img = self.transform(img)  # 是否进行transform
+        return img, label# return很关键，return回哪些内容，那么我们在训练时循环读取每个batch时，就能获得哪些内容
+
+    def __len__(self):  # 这个函数也必须要写，它返回的是数据集的长度，也就是多少张图片，要和loader的长度作区分
+        return len(self.imgs)
+
+
+
+if __name__ == "__main__":
+    transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    # 根据自己定义的那个勒MyDataset来创建数据集！注意是数据集！而不是loader迭代器
+    train_data = MyDataset(r"F:/gitpython/pytorch/surgical_dataset_4classes/train/",transform)
+    train_load  = torch.utils.data.DataLoader(train_data, batch_size=3,shuffle=True, num_workers=2)
+    class_list = ["forceps1/", "scissors1/", "scissors2/", "tweezers/"]
+    # 获取随机数据
+    dataiter = iter(train_load)
+    images, labels = dataiter.next()
+
+    images = torchvision.utils.make_grid(images)
+    images = images / 2 + 0.5  # unnormalize
+    npimg = images.numpy()
+
+
--- pytorch/surgical_dataset_4classes/test.py
+++ pytorch/surgical_dataset_4classes/test.py
@@ -4,9 +4,14 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 import torchvision
-class Op_net(nn.Module):
+import torch.optim as optim
+import torchvision.transforms as transforms
+import os
+from newdata import MyDataset
+
+class SimpleNet(nn.Module):
     def __init__(self):
-        super(Op_net, self).__init__()
+        super(SimpleNet, self).__init__()
 
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=12, kernel_size=3, padding=1)
         self.relu1 = nn.LeakyReLU()
@@ -16,64 +21,109 @@ class Op_net(nn.Module):
 
         self.pool1 = nn.MaxPool2d(kernel_size=2)
 
-        self.conv3 = nn.Conv2d(in_channels=24, out_channels=24, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=24, out_channels=128, kernel_size=3, padding=1)
         self.relu3 = nn.LeakyReLU()
 
-        self.conv4 = nn.Conv2d(in_channels=24, out_channels=24, kernel_size=3, padding=1)
+        self.conv4 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1)
         self.relu4 = nn.LeakyReLU()
 
         self.pool2 = nn.MaxPool2d(kernel_size=2)
 
-        self.fc = nn.Linear(in_features=128 * 128 * 24, out_features=4)
+        self.fc1 = nn.Linear(in_features=128 * 128 * 128, out_features=84)
+        self.relu5 = nn.LeakyReLU()
+
+        self.fc2 = nn.Linear(in_features=84, out_features=24)
+        self.relu6 = nn.LeakyReLU()
+
+        self.fc3 = nn.Linear(in_features=24, out_features=4)
+
 
     def forward(self, input_nb):
-        self.imshow(torchvision.utils.make_grid(input_nb[0][0].detach()))
 
         output = self.conv1(input_nb)
         output = self.relu1(output)
 
-        self.imshow(torchvision.utils.make_grid(output[0][0].detach()))
-
         output = self.conv2(output)
         output = self.relu2(output)
 
-        self.imshow(torchvision.utils.make_grid(output[0][0].detach()))
-
         output = self.pool1(output)
-
-        self.imshow(torchvision.utils.make_grid(output[0][0].detach()))
 
         output = self.conv3(output)
         output = self.relu3(output)
 
-        self.imshow(torchvision.utils.make_grid(output[0][0].detach()))
-
-        output = self.conv4(output)
-        output = self.relu4(output)
-
-        self.imshow(torchvision.utils.make_grid(output[0][0].detach()))
+        # output = self.conv4(output)
+        # output = self.relu4(output)
 
         output = self.pool2(output)
+        # 特征展平
+        output = output.view(-1, 128 * 128 * 128)
 
-        self.imshow(torchvision.utils.make_grid(output[0][0].detach()))
+        output = self.fc1(output)
+        output = self.relu5(output)
 
-        output = output.view(-1, 128*128*24)
+        output = self.fc2(output)
+        output = self.relu6(output)
 
-        output = self.fc(output)
+        output = self.fc3(output)
 
         return output
 
-    def imshow(self,img):
-        img = img / 2 + 1  # unnormalize
-        npimg = img.numpy()
-        plt.imshow(np.transpose(npimg, (1, 2, 0)))
-        plt.show()
+
+
+
 if __name__ == "__main__":
-    img = cv.imread(r"F:\gitpython\pytorch\surgical_dataset_4classes\train\forceps1\IMG_20191014_203948.jpg")
-    res = cv.resize(img, (512, 512))
-    cv.imshow("s",res)
-    res.resize((1,3,512,512))
-    res = torch.Tensor(res)
-    net = Op_net()
-    output = net.forward(res)
-    print(output)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    loss_list = []
+    count = []
+    num = 0
+    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    # 根据自己定义的那个勒MyDataset来创建数据集！注意是数据集！而不是loader迭代器
+    train_data = MyDataset(r"E:/gaoxinzhi/pytorch/surgical_dataset_4classes/train/", transform)
+    trainloader = torch.utils.data.DataLoader(train_data, batch_size=5, shuffle=True, num_workers=2)
+    class_list = ["forceps1", "scissors1", "scissors2", "tweezers"]
+    # 获取随机数据
+    dataiter = iter(trainloader)
+    images, labels = dataiter.next()
+
+    network = SimpleNet().to(device)
+    criterion = nn.CrossEntropyLoss()
+    tran = transforms.ToTensor()
+    optimizer = optim.SGD(network.parameters(),lr=0.0008,momentum=0.7)
+    network.train()
+    print("begin training")
+
+    for epoch in range(20):  # 多批次循环
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # 获取输入
+            inputs, labels = data
+            inputs, labels = inputs.to(device),labels.to(device)
+            # 梯度置0
+            optimizer.zero_grad()
+            # 正向传播，反向传播，优化
+            outputs = network(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            # 打印状态信息
+            running_loss += loss.item()
+            if i % 20 == 19:  # 每20批次打印一次
+                print('[%d, %5d] loss: %.3f' %(epoch + 1, i + 1, running_loss / 20))
+                loss_list.append(running_loss/20)
+                running_loss = 0.0
+                count.append(num)
+                num += 1
+    plt.figure(1)
+    plt.title("loss - frequency")
+    plt.ylabel("loss")
+    plt.xlabel("training frequency")
+    plt.grid(True)
+    x = np.array(count)
+    y = np.array(loss_list)
+    plt.plot(x,y)
+    plt.show()
+    print('Finished Training')
+    print('Finished Training begin save mode')
+    PATH = r'./shit_net.pth'
+    torch.save(network.state_dict(), PATH)
+    print("model save finished")
