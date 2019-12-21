4ff16d101ed5c1adff4005134380909d0163d345
DUT_gaoxinzhi <651599228@qq.com>
2019-12-21 15:49:56 +0800

    update dataset so that it can run

--- pytorch/surgical_dataset_4classes/image_change.py
+++ pytorch/surgical_dataset_4classes/image_change.py
@@ -1,9 +1,18 @@
 import cv2 as cv
-
-img = cv.imread(r"F:\gitpython\pytorch\surgical_dataset_4classes\train\forceps1\IMG_20191014_203948.jpg")
-res = cv.resize(img,(512,512))
-while True:
-    k = cv.waitKey(5)
-    cv.imshow("x", res)
-    if k == 'q':
-        cv.destroyAllWindows()
+import os
+train_root  = r"F:/gitpython/pytorch/surgical_dataset_4classes/train/"
+test_root  = r"F:/gitpython/pytorch/surgical_dataset_4classes/test/"
+class_list = ["forceps1/", "scissors1/", "scissors2/", "tweezers/"]
+count = 0
+for name in class_list:
+    data_path = test_root+name
+    # 获取输入
+    for img in os.listdir(data_path):
+        img_path = data_path + img
+        img_now = cv.imread(img_path)
+        # 修改大小并写回文件
+        res = cv.resize(img_now, (512, 512))
+        cv.imwrite(img_path,res)
+        count += 1
+        if(count%20 == 0):
+            print(count)
---
+++ pytorch/surgical_dataset_4classes/train.py
@@ -0,0 +1,48 @@
+import torch
+import torch.nn as nn
+from test import SimpleNet
+import os
+import numpy as np
+import cv2
+import torchvision.transforms as transforms
+import torchvision
+from newdata import MyDataset
+import matplotlib.pyplot as plt
+if __name__ == "__main__":
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    # 根据自己定义的那个勒MyDataset来创建数据集！注意是数据集！而不是loader迭代器
+    test_data = MyDataset(r"E:/gaoxinzhi/pytorch/surgical_dataset_4classes/test/", transform)
+    testloader = torch.utils.data.DataLoader(test_data, batch_size=4, shuffle=True, num_workers=2)
+    class_list = ["forceps1", "scissors1", "scissors2", "tweezers"]
+    PATH = r'./shit_net.pth'
+    net = SimpleNet().to(device)
+    net.load_state_dict(torch.load(PATH))
+    correct = 0
+    total = 0
+    accuarcy = []
+    frequency = []
+    with open(r"./result.csv", 'w') as f:
+        with torch.no_grad():
+            for data in testloader:
+                images, labels = data
+                images, labels = images.to(device), labels.to(device)
+                outputs = net(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+                for i in range(4):
+                    f.write(class_list[labels[i]] + "\t" + class_list[predicted[i]] + '\n')
+                accuarcy.append(correct / total)
+                frequency.append(total)
+    plt.figure(1)
+    plt.title("accuaracy - frequency")
+    plt.ylabel("accuaracy")
+    plt.xlabel("frequency")
+    plt.grid(True)
+    y = np.array(accuarcy)
+    x = np.array(frequency)
+    plt.plot(x,y)
+    plt.show()
+    print('Accuracy of the network on the test images: %d %%' % (100 * correct / total))
+
